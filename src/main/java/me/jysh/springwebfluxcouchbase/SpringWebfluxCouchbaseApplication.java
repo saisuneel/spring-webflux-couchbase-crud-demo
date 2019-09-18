8662bff6dc169c2821d742c61e9e933b3aaa858b
Jayesh Patel <jayesh.patel1@globallogic.com>
2019-09-18 10:50:21 +0530

    Initial Project Setup

---
+++ src/main/java/me/jysh/springwebfluxcouchbase/SpringWebfluxCouchbaseApplication.java
@@ -0,0 +1,13 @@
+package me.jysh.springwebfluxcouchbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxCouchbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxCouchbaseApplication.class, args);
+    }
+
+}
